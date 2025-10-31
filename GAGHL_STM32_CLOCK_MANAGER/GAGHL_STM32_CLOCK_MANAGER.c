4275e0a59523be2200fe095953f2d315b0338c9d
GAGHL <ns.gaghl@gmail.com>
2025-10-31 19:47:09 +0330

    Fix somethings

--- GAGHL_STM32_CLOCK_MANAGER/GAGHL_STM32_CLOCK_MANAGER.c
+++ GAGHL_STM32_CLOCK_MANAGER/GAGHL_STM32_CLOCK_MANAGER.c
@@ -4,75 +4,38 @@
 #include <main.h>
 
 volatile CLOCK_SOURCE_T current_clock_source;
+volatile PLL_CLOCK_SOURCE_T current_pll_clock_source;
 
 void Check_Clock_Source(void) {
 	uint32_t clock_source = (RCC->CFGR >> 2) & 0x3;
-	uint32_t pll_source = (RCC->CFGR >> 16) & 0x1;
 
 	switch (clock_source) {
 		case 0x00:
-			current_clock_source = Clock_Source_HSI;
+			current_clock_source = Clock_Source_is_HSI;
 			break;
 
 		case 0x01:
-			current_clock_source = Clock_Source_HSE;
+			current_clock_source = Clock_Source_is_HSE;
 			break;
 
 		case 0x02:
-			if (pll_source == 0x01) {
-				current_clock_source = PLL_Source_HSE;
-			} else {
-				current_clock_source = PLL_Source_HSI_div_2;
-			}
+			current_clock_source = Clock_Source_is_PLL;
 			break;
 	}
 }
 
-static inline void CSS_TURN_ON(void) { RCC->CR |= RCC_CR_CSSON; }
+void PLL_check_Clock_Source(void) {
+	uint32_t pll_source = (RCC->CFGR >> 16) & 0x1;
 
-static inline void HSI_TURN_ON(void) { RCC->CR |= RCC_CR_HSION; }
-
-static inline void HSE_TURN_ON(void) { RCC->CR |= RCC_CR_HSEON; }
-
-static inline void PLL_TURN_ON(void) { RCC->CR |= RCC_CR_PLLON; }
-
-static inline void SET_PLL_MULTIPLIER_mull_2(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL2;  }
-static inline void SET_PLL_MULTIPLIER_mull_3(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL3;  }
-static inline void SET_PLL_MULTIPLIER_mull_4(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL4;  }
-static inline void SET_PLL_MULTIPLIER_mull_5(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL5;  }
-static inline void SET_PLL_MULTIPLIER_mull_6(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL6;  }
-static inline void SET_PLL_MULTIPLIER_mull_7(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL7;  }
-static inline void SET_PLL_MULTIPLIER_mull_8(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL8;  }
-static inline void SET_PLL_MULTIPLIER_mull_9(void)  { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL9;  }
-static inline void SET_PLL_MULTIPLIER_mull_10(void) { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL10; }
-static inline void SET_PLL_MULTIPLIER_mull_11(void) { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL11; }
-static inline void SET_PLL_MULTIPLIER_mull_12(void) { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL12; }
-static inline void SET_PLL_MULTIPLIER_mull_13(void) { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL13; }
-static inline void SET_PLL_MULTIPLIER_mull_14(void) { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL14; }
-static inline void SET_PLL_MULTIPLIER_mull_15(void) { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL15; }
-static inline void SET_PLL_MULTIPLIER_mull_16(void) { RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL16; }
-
-static inline void SET_PLL_HSE_PRESCALER_div_1(void) { RCC->CFGR &= ~RCC_CFGR_PLLXTPRE; }
-static inline void SET_PLL_HSE_PRESCALER_div_2(void) { RCC->CFGR |=  RCC_CFGR_PLLXTPRE; }
-
-static inline void SET_PLL_SOURCE_HSI(void) { RCC->CFGR &= ~RCC_CFGR_PLLSRC; }
-static inline void SET_PLL_SOURCE_HSE(void) { RCC->CFGR |= RCC_CFGR_PLLSRC; }
-
-static inline void SET_SYSTEM_CLOCK_SOURCE_HSI(void) { RCC->CFGR |= RCC_CFGR_SW_HSI; }
-static inline void SET_SYSTEM_CLOCK_SOURCE_HSE(void) { RCC->CFGR |= RCC_CFGR_SW_HSE; }
-static inline void SET_SYSTEM_CLOCK_SOURCE_PLL(void) { RCC->CFGR |= RCC_CFGR_SW_PLL; }
-//-------------------------------------------------
-static inline void HSI_ON_AND_STABLAZITION(void) {
-	HSI_TURN_ON();
-	while (!(RCC->CR & RCC_CR_HSIRDY));
+	if (((RCC->CFGR >> 2) & 0x3) == (0x02)) {
+		if (pll_source == 0x01) {
+			current_pll_clock_source = PLL_Source_is_HSE;
+		} else {
+			current_pll_clock_source = PLL_Source_is_HSI_div2;
+		}
+	} else {
+		current_pll_clock_source = PLL_is_Disable;
+	}
 }
 
-static inline void HSE_ON_AND_STABLAZITION(void) {
-	HSE_TURN_ON();
-	while (!(RCC->CR & RCC_CR_HSERDY));
-}
 
-static inline void PLL_ON_AND_STABLAZITION(void) {
-	PLL_TURN_ON();
-	while (!(RCC->CR & RCC_CR_PLLRDY));
-}
--- GAGHL_STM32_CLOCK_MANAGER/GAGHL_STM32_CLOCK_MANAGER.h
+++ GAGHL_STM32_CLOCK_MANAGER/GAGHL_STM32_CLOCK_MANAGER.h
@@ -1,58 +1,73 @@
 #ifndef GAGHL_STM32_CLOCK_MANAGER_H
 #define GAGHL_STM32_CLOCK_MANAGER_H
 
+#include <stdint.h>
+#include <main.h>
+
 typedef enum {
-	Clock_Source_HSI = 0,
-	Clock_Source_HSE,
-	Clock_Source_PLL,
-	PLL_Source_HSE,
-	PLL_Source_HSI_div_2
+	Clock_Source_is_HSI = 0,
+	Clock_Source_is_HSE,
+	Clock_Source_is_PLL
 } CLOCK_SOURCE_T;
+
+typedef enum {
+	PLL_is_Disable = 0,
+	PLL_Source_is_HSE,
+	PLL_Source_is_HSI_div2
+} PLL_CLOCK_SOURCE_T;
 
 void Check_Clock_Source(void);
 
-static inline void CSS_TURN_ON(void);
-static inline void HSI_TURN_ON(void);
-static inline void HSE_TURN_ON(void);
-static inline void PLL_TURN_ON(void);
+static inline void RCC_Enable_CSS(void) { RCC->CR |= RCC_CR_CSSON; }
+static inline void RCC_Enable_HSI(void) { RCC->CR |= RCC_CR_HSION; }
+static inline void RCC_Enable_HSE(void) { RCC->CR |= RCC_CR_HSEON; }
+static inline void RCC_Enable_PLL(void) { RCC->CR |= RCC_CR_PLLON; }
 
-static inline void SET_PLL_MULTIPLIER_mull_2(void);
-static inline void SET_PLL_MULTIPLIER_mull_3(void);
-static inline void SET_PLL_MULTIPLIER_mull_4(void);
-static inline void SET_PLL_MULTIPLIER_mull_5(void);
-static inline void SET_PLL_MULTIPLIER_mull_6(void);
-static inline void SET_PLL_MULTIPLIER_mull_7(void);
-static inline void SET_PLL_MULTIPLIER_mull_8(void);
-static inline void SET_PLL_MULTIPLIER_mull_9(void);
-static inline void SET_PLL_MULTIPLIER_mull_10(void);
-static inline void SET_PLL_MULTIPLIER_mull_11(void);
-static inline void SET_PLL_MULTIPLIER_mull_12(void);
-static inline void SET_PLL_MULTIPLIER_mull_13(void);
-static inline void SET_PLL_MULTIPLIER_mull_14(void);
-static inline void SET_PLL_MULTIPLIER_mull_15(void);
-static inline void SET_PLL_MULTIPLIER_mull_16(void);
+static inline void RCC_Set_PLL_Multiplier_To_2(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL2;  }
+static inline void RCC_Set_PLL_Multiplier_To_3(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL3;  }
+static inline void RCC_Set_PLL_Multiplier_To_4(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL4;  }
+static inline void RCC_Set_PLL_Multiplier_To_5(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL5;  }
+static inline void RCC_Set_PLL_Multiplier_To_6(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL6;  }
+static inline void RCC_Set_PLL_Multiplier_To_7(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL7;  }
+static inline void RCC_Set_PLL_Multiplier_To_8(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL8;  }
+static inline void RCC_Set_PLL_Multiplier_To_9(void)  { RCC->CFGR |= RCC_CFGR_PLLMUL9;  }
+static inline void RCC_Set_PLL_Multiplier_To_10(void) { RCC->CFGR |= RCC_CFGR_PLLMUL10; }
+static inline void RCC_Set_PLL_Multiplier_To_11(void) { RCC->CFGR |= RCC_CFGR_PLLMUL11; }
+static inline void RCC_Set_PLL_Multiplier_To_12(void) { RCC->CFGR |= RCC_CFGR_PLLMUL12; }
+static inline void RCC_Set_PLL_Multiplier_To_13(void) { RCC->CFGR |= RCC_CFGR_PLLMUL13; }
+static inline void RCC_Set_PLL_Multiplier_To_14(void) { RCC->CFGR |= RCC_CFGR_PLLMUL14; }
+static inline void RCC_Set_PLL_Multiplier_To_15(void) { RCC->CFGR |= RCC_CFGR_PLLMUL15; }
+static inline void RCC_Set_PLL_Multiplier_To_16(void) { RCC->CFGR |= RCC_CFGR_PLLMUL16; }
 
-static inline void SET_PLL_HSE_PRESCALER_div_1(void);
-static inline void SET_PLL_HSE_PRESCALER_div_2(void);
+static inline void RCC_Set_PLL_Clock_Source_To_HSI_Div2(void) {
+	RCC->CFGR &= ~RCC_CFGR_PLLSRC;
+}
+static inline void RCC_Set_PLL_Clock_Source_To_HSE_Div1(void) {
+	RCC->CFGR |= RCC_CFGR_PLLSRC;
+	RCC->CFGR &= ~RCC_CFGR_PLLXTPRE;
+}
+static inline void RCC_Set_PLL_Clock_Source_To_HSE_Div2(void) {
+	RCC->CFGR |= RCC_CFGR_PLLSRC;
+	RCC->CFGR |=  RCC_CFGR_PLLXTPRE;
+}
 
-static inline void SET_PLL_SOURCE_HSI(void);
-static inline void SET_PLL_SOURCE_HSE(void);
+static inline void RCC_Set_System_Clock_Source_To_HSI_Output(void) { RCC->CFGR |= RCC_CFGR_SW_HSI; }
+static inline void RCC_Set_System_Clock_Source_To_HSE_Output(void) { RCC->CFGR |= RCC_CFGR_SW_HSE; }
+static inline void RCC_Set_System_Clock_Source_To_PLL_Output(void) { RCC->CFGR |= RCC_CFGR_SW_PLL; }
 
-static inline void SET_SYSTEM_CLOCK_SOURCE_HSI(void);
-static inline void SET_SYSTEM_CLOCK_SOURCE_HSE(void);
-static inline void SET_SYSTEM_CLOCK_SOURCE_PLL(void);
+static inline void RCC_Enable_HSI_And_Stabilization(void) {
+	RCC->CR |= RCC_CR_HSION;
+	while (!(RCC->CR & RCC_CR_HSIRDY));
+}
+static inline void RCC_Enable_HSE_And_Stabilization(void) {
+	RCC->CR |= RCC_CR_HSEON;
+	while (!(RCC->CR & RCC_CR_HSERDY));
+}
+static inline void RCC_Enable_PLL_And_Stabilization(void) {
+	RCC->CR |= RCC_CR_PLLON;
+	while (!(RCC->CR & RCC_CR_PLLRDY));
+}
 
-static inline void HSI_ON_AND_STABLAZITION(void);
-static inline void HSE_ON_AND_STABLAZITION(void);
-static inline void PLL_ON_AND_STABLAZITION(void);
 
-static inline void SET_HSI_CLOCK_SOURCE(void);
-
-
-static inline void SET_HSE_PREDIV(void);
-
-
-static inline void SET_PLL_CLOCK_SOURCE(void);
-static inline void SET_PLL_MULTIPLIER(void);
 
 #endif
